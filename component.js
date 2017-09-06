321b864bebc017723a85b92a2b5980215a65f740
newnowave <gdrapps@hotmail.com>
2017-09-05 21:54:52 -0700

    added players data in vue

--- component.js
+++ component.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#registration-form',
   data: {
+    players: [
+      "Quentin T. Tarantino",
+      "Martin D. Scorsese",
+      "Steven P. Spielberg",
+      "Christopher L. Nolan"
+    ],
     allSelectedNames: [],
     selected: '',
     stripName: function(selected){
