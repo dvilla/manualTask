8a1c06a9f2b0b6be858deda3657db2cb6f79e62e
newnowave <gdrapps@hotmail.com>
2017-09-05 21:51:43 -0700

    added list instead of textarea

--- component.js
+++ component.js
@@ -1,12 +1,13 @@
 var app = new Vue({
   el: '#registration-form',
   data: {
+    allSelectedNames: [],
     selected: '',
     stripName: function(selected){
       return selected.split(' ')
     },
-    addName: function(selected){
-      return console.log(selected);
+    addName: function(selected, allSelectedNames){
+      allSelectedNames.push(selected)
     }
   }
 })
