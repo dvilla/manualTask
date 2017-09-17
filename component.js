1b0a8c00630b83726ff5aa9bf0bfdaceacf476e5
newnowave <gdrapps@hotmail.com>
2017-09-16 19:23:39 -0700

    added functionality to add other players

--- component.js
+++ component.js
@@ -8,13 +8,22 @@ var app = new Vue({
       "Christopher L. Nolan",
       "David J. Fincher"
     ],
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    suffix: "",
     allSelectedNames: [],
     selected: '',
     stripName: function(){
       return this.selected.split(' ')
     },
-    addName: function(selected, allSelectedNames){
-      allSelectedNames.push(selected)
+    addName: function(){
+      if ( this.selected != "other" && this.selected != "" ) {
+        this.allSelectedNames.push(this.selected);
+      }
+      else {
+        this.allSelectedNames.push(this.firstName + " " + this.middleName + " " + this.lastName);
+      }
     }
   },
   computed: {
